036e2cbe97c5d7766d2331507d16582493d14120
Anne-Victoria Meyer <2meyer@informatik.uni-hamburg.de>
2018-07-07 23:21:13 +0200

    basic react component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function TimeInput(props) {
+  return <p>I came to work at XX:XX</p>;
+}
+
+function DurationInput(props) {
+  return <p>My break today was XX:XX</p>;
+}
+
+function TimeInputArea(props) {
+  return (
+    <div>
+      <TimeInput />
+      <TimeInput />
+      <DurationInput />
+    </div>
+  );
+}
+
+function TimeOutput(props) {
+  return <p>You worked XX:XX hours today</p>;
+}
+
+class WorkDurationCalculator extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <TimeInputArea />
+        <TimeOutput />
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello React
+        <WorkDurationCalculator />
       </div>
     );
   }
